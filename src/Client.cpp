978ad1e4a976a1553cbfd8f7a844f0968dfbe9df
LuisAlexisSalazar <luisalexis30616031@gmail.com>
2021-06-04 17:55:19 -0500

    MainServer and CLient

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,9 +1,13 @@
+// Client.cpp
 #include <iostream>
+#include "Network/Socket.hpp"
 #include "App/App.hpp"
 
 int main(int argc, char *argv[])
-{    
+{
   app::Client clientInstance("34.94.147.12", "8000");
+  //RDT::UdpSocket clientSocket("34.94.147.12", "8000");
+
   if(argc > 1)
   {
     if(!clientInstance.setCommand(argv[1]))
@@ -12,5 +16,7 @@ int main(int argc, char *argv[])
       return EXIT_FAILURE;
     }
   }
+  //clientSocket.send();
+
   return EXIT_SUCCESS;
 }
--- src/MainServer.cpp
+++ src/MainServer.cpp
@@ -1,16 +1,13 @@
+// MainServer.cpp
 #include "Network/Socket.hpp"
-
 int main(){
-  RDT::UdpSocket listener("", "8000");
-  RDT::UdpSocket slaveServerSocket("34.68.1.20", "8000");
-  
+  RDT::UdpSocket mainServerListener("", "8000");
+  RDT::UdpSocket slaveServerSocket("35.188.208.43", "8000");
   std::string received_message, IP_from;
   uint16_t Port_from;
-
   // recibes el mensaje desde el cliente
-  listener.receive(received_message, IP_from, Port_from);
+  mainServerListener.receive(received_message, IP_from, Port_from);
   // envías el mensaje al servidor esclavo
   slaveServerSocket.send(received_message);
-
   return 0;
 }
