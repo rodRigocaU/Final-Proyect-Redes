f39afd6328e1a9802c3b8b5fbc11b6bf47a3ced1
Lonehandyman <italomh2000@gmail.com>
2021-06-17 11:52:10 -0500

    Sha256

--- include/Network/RDTMask.hpp
+++ include/Network/RDTMask.hpp
@@ -6,29 +6,32 @@
 
 #include "UdpSocket.hpp"
 
+#define HASH_BYTE_SIZE        64
+#define ALTERBIT_BYTE_SIZE    1
+#define ALTERBIT_LOWERBOUND   1
+#define ALTERBIT_UPPERBOUND   2
+#define MSG_BYTE_SIZE         4
+#define RDT_HEADER_BYTE_SIZE  ALTERBIT_BYTE_SIZE + HASH_BYTE_SIZE + MSG_BYTE_SIZE
+
 namespace rdt
 {  
-  /*
-    00000001 + 00100101 + 0996 + message
-    alter + checksum + msg_size + message
 
-    Delete RdtEncoder & Decoder turning them to methods in RDTSocket [v]
-  */
   class RDTSocket{
   private:
+    uint8_t alterBit = ALTERBIT_LOWERBOUND;
     std::unique_ptr<net::UdpSocket> mainSocket;
-    
-    uint8_t switchBitAlternate();
 
-    u_char getChecksum(const std::string& message);
-    
+    uint8_t switchBitAlternate();
+    bool isCorrupted(const std::string& message, const std::string& hash);
     void encode(std::string& message);
-    
-    void decode(std::string& message); 
+    bool decode(std::string& message); 
 
   public:
-    void send(const std::string& message);
-    void receive();
+    enum Status {Done, Wait, Disconnected, Error};
+
+    void setReceptorSettings(const std::string& IpAddress, const uint16_t& port);
+    Status send(const std::string& message);
+    Status receive(std::string& message, std::string& remoteIp, uint16_t& remotePort);
   };
 
 }
--- src/Network/RDTMask.cpp
+++ src/Network/RDTMask.cpp
@@ -1,3 +1,40 @@
 #include "Network/RDTMask.hpp"
+#include "Network/Algorithm/sha256.hpp"
+#include "App/Tools/Fixer.hpp"
 
 using namespace rdt;
+
+uint8_t RDTSocket::switchBitAlternate(){
+  uint8_t tempMod = ALTERBIT_UPPERBOUND + 1 - ALTERBIT_LOWERBOUND;
+  alterBit = (alterBit - ALTERBIT_LOWERBOUND + 1) % tempMod;
+  return alterBit += ALTERBIT_LOWERBOUND;
+}
+
+bool RDTSocket::isCorrupted(const std::string& message, const std::string& hash){
+  return crypto::sha256(message) == hash;
+}
+
+void RDTSocket::encode(std::string& message){
+  std::string sha256Hash = crypto::sha256(message);
+  std::string messageSize = tool::fixToBytes(std::to_string(message.length()), 4);
+  message = std::to_string(alterBit) + sha256Hash + messageSize + message;
+  switchBitAlternate();
+}
+
+bool RDTSocket::decode(std::string& message){
+  std::string header = message.substr(0, RDT_HEADER_BYTE_SIZE);
+  message = message.substr(RDT_HEADER_BYTE_SIZE);
+  return (std::stoi(header.substr(0, ALTERBIT_BYTE_SIZE)) == alterBit) &&
+  (header.substr(ALTERBIT_BYTE_SIZE, HASH_BYTE_SIZE) == crypto::sha256(message));
+}
+
+void RDTSocket::setReceptorSettings(const std::string& IpAddress, const uint16_t& port){
+  mainSocket = std::make_unique<net::UdpSocket>(IpAddress, port);
+}
+
+RDTSocket::Status RDTSocket::send(const std::string& message){
+  return Done;
+}
+RDTSocket::Status RDTSocket::receive(std::string& message, std::string& remoteIp, uint16_t& remotePort){
+  return Done;
+}
