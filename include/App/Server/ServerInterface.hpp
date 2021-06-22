f48a85b65fa304316ced8971ad925423cb035ad0
politeperson <saul.rojas@ucsp.edu.pe>
2021-06-22 18:48:22 -0500

    merging

--- include/App/Server/ServerInterface.hpp
+++ include/App/Server/ServerInterface.hpp
@@ -16,57 +16,6 @@
 namespace app
 {
 
-  class MainServerApp {
-    private:
-    int port;
-    std::string ip;
-    struct sockaddr_in stSockAddr;
-    int connection;
-    int server;
-    int socketServer;
-    int socketCliente;
-    int socketRepository; // se usa en el servidor respositorio
-    char mode;
-    int numberRepositories;
-    int repository;
-    
-    int mainPort;
-    std::string mainIp;
-
- 
-    std::map<int,std::string>   connections;
-    std::vector<int> socketRepositories; // se usa en el maestro
-
-    void runMainServer();
-    void connMasterRepository(int pPort, string pIp);
-
-  public:
-    void runRepository();
-    
-    void setMainIp(string ip){mainIp = ip;};
-    void setMainPort(int port){mainPort = port;};
-    void setMode(char Pmode='S') { mode = Pmode;}
-    void setPort(int Pport=50000) { port = Pport;}
-    void setIp(string Pip="127.0.0.1") { ip = Pip;}
-    void setNumberRepositories(int r){numberRepositories = r;}
-    void registerRepository();   
-  };
-
-  void MainServerApp::runMainServer()
-  {
-    for(;;){
-      int newCo.
-      nnection = accept(socketServer, NULL, NULL);
-      if(0 > newConnection){
-        perror("error accept failed");
-        close(newConnection);
-        exit(EXIT_FAILURE);
-      }
-    //std::thread (&DGDB::runConnection,this,newConnection).detach();
-    }
-  }
- 
-
 }
 
 
--- src/Slave.cpp
+++ src/Slave.cpp
@@ -10,29 +10,29 @@ int main()
   std::string received_comand, IP_from;
   uint16_t Port_from;
 
-  db::SQLite BD;
-  BD.createTables();
+  // db::SQLite BD;
+  // BD.createTables();
   
-  slaveServerSocket.receive(received_comand, IP_from, Port_from);
-  std::cout << received_comand << " " << IP_from << " " << Port_from << '\n';
+  // slaveServerSocket.receive(received_comand, IP_from, Port_from);
+  // std::cout << received_comand << " " << IP_from << " " << Port_from << '\n';
 
-  std::string name_nodo;
-  std::vector<std::pair<std::string, std::string>> attributes;
+  // std::string name_nodo;
+  // std::vector<std::pair<std::string, std::string>> attributes;
 
-  msg::CreateNodePacket c;
-  c << received_comand;
-  std::cout << c.nodeId << '\n';
+  // msg::CreateNodePacket c;
+  // c << received_comand;
+  // std::cout << c.nodeId << '\n';
 
-  for (auto atr : c.attributes)
-  {
-    attributes.push_back(atr);
-  }
+  // for (auto atr : c.attributes)
+  // {
+  //   attributes.push_back(atr);
+  // }
 
 
-  BD.Create(c.nodeId,attributes,c.relations);
-  BD.printSelectNodos();
-  BD.printSelectAttributes();
-  BD.printSelectRelations();
+  // BD.Create(c.nodeId,attributes,c.relations);
+  // BD.printSelectNodos();
+  // BD.printSelectAttributes();
+  // BD.printSelectRelations();
 
 
   
