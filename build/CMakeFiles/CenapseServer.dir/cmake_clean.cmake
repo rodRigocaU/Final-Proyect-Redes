dc8973c84d1b5ccbcc724fe177e40e4e5edfb27e
Lonehandyman <italomh2000@gmail.com>
2021-06-22 15:39:20 -0500

    Gaaaa

---
+++ build/CMakeFiles/CenapseServer.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CenapseServer.dir/src/App/Client/ClientInterface.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/Server/ServerInterface.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/Tools/Colors.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/Tools/Fixer.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/Tools/InterfacePerformance.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/TransportParser/Create.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/TransportParser/Delete.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/TransportParser/Read.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/App/TransportParser/Update.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/DataBase/Sqlite.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/DataBase/Tools.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/MainServer.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/Network/Algorithm/sha256.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/Network/RDTMask.cpp.o"
+  "CMakeFiles/CenapseServer.dir/src/Network/UdpSocket.cpp.o"
+  "CenapseServer"
+  "CenapseServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CenapseServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
