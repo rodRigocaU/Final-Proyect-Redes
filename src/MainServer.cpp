e87796bf8b6aa0d91e19503d86136da157bb74f5
politeperson <saul.rojas@ucsp.edu.pe>
2021-07-02 13:43:10 -0500

    añadida la secuencia de mensajes

--- src/MainServer.cpp
+++ src/MainServer.cpp
@@ -14,9 +14,17 @@ int main(){
   if(listener.accept(socket) != net::Status::Done){
     return EXIT_FAILURE;
   }
-  
-  
- 
+
+  std::cout << socket << std::endl;
+
+  while (true)
+  {
+    std::string msg;
+    socket.receive(msg);
+    std::cout << "mensaje recibido: " << msg << std::endl;
+  }
+  socket.passiveDisconnect(); // recibimos la desconexión del cliente
+
   /*net::UdpSocket slaveServerSocket("35.188.208.43", "8000");
 
   std::string received_message, IP_from;
