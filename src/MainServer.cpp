c1c550ed6be0c37388bb2d8d03e04e90d15d7b26
politeperson <saul.rojas@ucsp.edu.pe>
2021-07-02 13:40:39 -0500

    algunos cambios en el mainServer

--- src/MainServer.cpp
+++ src/MainServer.cpp
@@ -15,26 +15,7 @@ int main(){
     return EXIT_FAILURE;
   }
   
-  std::string msg;
-  std::cout << socket << std::endl;
-  socket.send("hola soy tu servidor");
-  socket.receive(msg);
-  std::cout << "==========MENSAJE: " << msg << std::endl;
-  std::cout << "+-+-+-++-+-+RECV 1\n";
-  socket.receive(msg);
-  std::cout << "==========MENSAJE: " << msg << std::endl;
-  std::cout << "+-+-+-++-+-+SEND 1\n";
-  socket.send("Te escuche");
-  std::cout << "+-+-+-++-+-+SEND 2\n";
-  socket.receive(msg);
-  std::cout << "==========MENSAJE: " << msg << std::endl;
-  std::cout << "+-+-+-++-+-+RECV 2\n";
-  socket.send(msg); // devolvemos el mensaje
-  socket.receive(msg);
-  std::cout << "==========MENSAJE: " << msg << std::endl;
-  std::cout << "+-+-+-+PROCEED TO DISCONNECT\n";
-  socket.passiveDisconnect();// recibimos la desconexión del cliente
- 
+  
  
   /*net::UdpSocket slaveServerSocket("35.188.208.43", "8000");
 
@@ -43,12 +24,9 @@ int main(){
   mainServerListener.receive(received_message, IP_from, Port_from);
   slaveServerSocket.send(received_message);*/
   /*
-  
   while(true){
     std::unique_ptr<RDTSocket> newSocket = std::make_unique<RDTSocket>();
-    
-  }
-
+  }    
   */
   return 0;
 }
