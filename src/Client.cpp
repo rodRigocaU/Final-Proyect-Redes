800bdc6073a49c1abe6160efb1cf914f18a161bc
politeperson <saul.rojas@ucsp.edu.pe>
2021-07-11 21:58:01 -0500

    cambios en la lectura de comandos y debugeo

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,13 +1,16 @@
 #include "App/Client/ClientInterface.hpp"
 
 int main(int argc, char *argv[]) {
+  
   std::map<std::string, std::string> requirements = {{"ServerMasterIp",""},{"ServerMasterClientPort",""}};
   if(!tool::readSettingsFile("Cenapse.conf", requirements, true)){
     tool::ConsolePrint("[Error]: Requirements missed.", RED);
     return EXIT_FAILURE;
   }
+  std::cout << argc << std::endl;
+  
   app::Client clientInstance(requirements["ServerMasterIp"], requirements["ServerMasterClientPort"]);
-
+  
   if(argc > 1) {
     if(!clientInstance.setCommand(argv[1])) {
       tool::ConsolePrint("[Error]: Can not process this command.", RED);
